6b3c8f72168f3addb25a3571f37ea84562bfab14
Bartosz Zielonka <batziell@gmail.com>
2024-07-28 17:01:39 +0200

    Improve logger

--- Perun/source/Core/Log.hpp
+++ Perun/source/Core/Log.hpp
@@ -22,5 +22,21 @@ namespace perun {
     };
 }
 
+// Core log macros
+#define PN_CORE_DEBUG(...)    ::perun::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define PN_CORE_TRACE(...)    ::perun::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define PN_CORE_INFO(...)     ::perun::Log::getCoreLogger()->info(__VA_ARGS__)
+#define PN_CORE_WARN(...)     ::perun::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define PN_CORE_ERROR(...)    ::perun::Log::getCoreLogger()->error(__VA_ARGS__)
+#define PN_CORE_CRITICAL(...) ::perun::Log::getCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define PN_TRACE(...)         ::perun::Log::getAppLogger()->trace(__VA_ARGS__)
+#define PN_TRACE(...)         ::perun::Log::getAppLogger()->trace(__VA_ARGS__)
+#define PN_INFO(...)          ::perun::Log::getAppLogger()->info(__VA_ARGS__)
+#define PN_WARN(...)          ::perun::Log::getAppLogger()->warn(__VA_ARGS__)
+#define PN_ERROR(...)         ::perun::Log::getAppLogger()->error(__VA_ARGS__)
+#define PN_CRITICAL(...)      ::perun::Log::getAppLogger()->critical(__VA_ARGS__)
+
 
 #endif //PERUN_LOG_HPP
--- Perun/source/main.cpp
+++ Perun/source/main.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
 #include "Core/Log.hpp"
 
 int main() {
     perun::Log::init();
-    perun::Log::getCoreLogger()->info("Hello World!");
-    perun::Log::getAppLogger()->info("Hello World!");
+
+    PN_CORE_INFO("TEST");
     return 0;
 }
